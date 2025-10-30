de83cb0949034741a0f5d6d7fabecd331b519fe2
Alberto De Lucca <albertodelucca@gmail.com>
2025-10-30 12:02:32 -0400

    Enhance Zsh completions setup
    - Ensure system and Homebrew zsh functions are included in the fpath to prevent "function definition file not found" errors in Cursor

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -4,6 +4,9 @@
 #                                                                              #
 ################################################################################
 
+# ensure system/Homebrew zsh functions are in path
+# fixes “function definition file not found” errors in Cursor
+fpath=(/usr/local/share/zsh/site-functions /usr/local/share/zsh/functions /usr/share/zsh/site-functions /usr/share/zsh/*/functions $fpath)
 
 # Add Docker CLI completions if they exist
 if [[ -d "$HOME/.docker/completions" ]]; then
